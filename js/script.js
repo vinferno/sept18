a1be7bc8794683da69f02c5e7d6bd99d8d5d26ff
vinferno <vinsonfernandez@gmail.com>
2015-09-18 21:27:35 +0000

    text

--- js/script.js
+++ js/script.js
@@ -200,15 +200,16 @@ var fu_touch_end = function(event){
 	}
 	if (start_swipe > end_swipe + threshold){
 		//alert('swipe left')
-		for (var i = 0; i<games.length;i++){
-			if (games[i].children.length === 2){
-				var new_string =games[i].children[0].value;
-				games[i].innerHTML='';
-				games[i].innerText=new_string;
-			}
-		}
+
 		if(global_target.innerText === "RENAME"){return;}
 		if (global_target.classList.contains('game_list_item')){
+			for (var i = 0; i< games.length; i++) {
+				if (games[i].children.length === 2){
+					var new_string =games[i].children[0].value;
+					games[i].innerHTML='';
+					games[i].innerText=new_string;
+				}
+			}
 		var new_text_box = document.createElement('input');
 		var new_button = document.createElement('button');
 		new_text_box.type = "text";
